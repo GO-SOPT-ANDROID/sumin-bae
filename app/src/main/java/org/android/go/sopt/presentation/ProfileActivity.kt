58415523b01c7c69196587b464d30b2bfbbb0a66
waterminn <tnals08019@gmail.com>
2023-04-05 18:32:51 +0900

    로그인, 회원가입, 자기소개 액티비티 생성

---
+++ app/src/main/java/org/android/go/sopt/presentation/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package org.android.go.sopt.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.android.go.sopt.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
