824586824c9d0c68f92bd59988a0a141ce039245
waterminn <tnals08019@gmail.com>
2023-04-21 13:13:37 +0900

    #2 [ui] 팔레트 페이지 ui 구성

--- app/src/main/java/org/android/go/sopt/presentation/home/color/ColorListFragment.kt
+++ app/src/main/java/org/android/go/sopt/presentation/home/color/ColorListFragment.kt
@@ -1,9 +1,9 @@
-package org.android.go.sopt.presentation.home.follower
+package org.android.go.sopt.presentation.home.color
 
 import org.android.go.sopt.databinding.FragmentFollowerBinding
 import org.android.go.sopt.util.binding.ViewBindingFragment
 
-class FollowerFragment :
+class ColorListFragment :
     ViewBindingFragment<FragmentFollowerBinding>(FragmentFollowerBinding::inflate) {
 
 }
