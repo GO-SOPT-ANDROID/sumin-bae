b29ff1406937fe46d495a8b2978ab024ea464018
waterminn <tnals08019@gmail.com>
2023-05-06 15:12:50 +0900

    viewpager2 구현

--- app/src/main/java/org/android/go/sopt/presentation/home/gallery/GalleryFragment.kt
+++ app/src/main/java/org/android/go/sopt/presentation/home/gallery/GalleryFragment.kt
@@ -1,9 +1,17 @@
 package org.android.go.sopt.presentation.home.gallery
 
+import android.os.Bundle
+import android.view.View
+import org.android.go.sopt.R
 import org.android.go.sopt.databinding.FragmentGalleryBinding
 import org.android.go.sopt.util.binding.ViewBindingFragment
 
 class GalleryFragment :
     ViewBindingFragment<FragmentGalleryBinding>(FragmentGalleryBinding::inflate) {
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.vpGallery.adapter = GalleryAdapter().apply {
+            setItemList(listOf(R.drawable.arale_profile, R.drawable.ic_launcher_background))
+        }
+    }
 }
